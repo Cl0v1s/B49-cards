19372c58fca2272fb1ce4f65bab8d1f2257eea80
Clovis <portron.cl@gmail.com>
2020-03-12 09:18:45 +0100

    Ajout scan

--- app/www/src/Discover.js
+++ app/www/src/Discover.js
@@ -9,14 +9,43 @@ class Discover {
 
     async scan() {
         console.log('starting scan');
-        console.log(window.QRScanner);
+        cordova.plugins.barcodeScanner.scan(
+            function (result) {
+                alert("We got a barcode\n" +
+                    "Result: " + result.text + "\n" +
+                    "Format: " + result.format + "\n" +
+                    "Cancelled: " + result.cancelled);
+            },
+            function (error) {
+                alert("Scanning failed: " + error);
+            }
+        );
+
+        /*console.log(window.QRScanner);
         if(window.QRScanner) {
-            const text = await new Promise(resolve => QRScanner.scan((err, text) => resolve(text)) && QRScanner.show());
+            const text = await new Promise((resolve) => {
+                var done = function(err, status){
+                    if(err){
+                      console.error(err._message);
+                    } else {
+                      console.log('QRScanner is initialized. Status:');
+                      console.log(status);
+                    }
+                  };
+                  
+                QRScanner.prepare(done);
+                QRScanner.show();
+                QRScanner.getStatus(function(status){
+                    console.log(status);
+                });
+                QRScanner.scan((err, text) => resolve(text));
+            });
             alert(text);
         } else {
             this.devices = ["clovis.portron", "clovis.portron"];
-        }
-        
+        }*/
+
+
         console.log('scan done');
     }
 }
