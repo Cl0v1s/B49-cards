3c035f7e079ed681b304c19b71d041b1c786848b
Clovis <portron.cl@gmail.com>
2020-03-15 21:41:38 +0100

    Ajout service worker cache

--- app/www/src/index.js
+++ app/www/src/index.js
@@ -5,18 +5,23 @@ import '@fortawesome/fontawesome-free/scss/solid.scss';
 import Discover from './Discover';
 import CardFactory from './CardFactory';
 
-import {init } from './ui/HomeView';
+import {
+    init
+} from './ui/HomeView';
 
 window.addEventListener('load', async () => {
     const site = "https://clovis-portron.github.io/B49-cards/site";
     CardFactory.setSite(site);
 
-    /*if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('/service-worker.js')
-        .then((reg) => {
-            console.log('Service worker registered.', reg);
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.register('./src/cache-service.js').then(function (registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function (err) {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
         });
-    }*/
+    }
 
     const home = document.querySelector('b49-home');
     init(home);
